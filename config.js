84d961462831136be99721c5bef9e61cc62554d1
fyupanquia <fyupanquia@outlook.com>
2020-10-28 21:55:10 -0500

    much better

---
+++ config.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+
+module.exports = {
+  api: {
+    port: process.env.API_PORT || 3000,
+  },
+  jwt: {
+    secret: process.env.JWT_SECRET || "secret",
+  },
+  mysql: {
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+  },
+};
